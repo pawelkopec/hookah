811f9a9d9f09a13abe6303e186b6710e6b286449
pawelkopec <pawelkopec10@gmail.com>
2017-07-18 23:07:11 +0200

    Add newline after class definition

--- src/main/scala/core/ArticleAnalyser.scala
+++ src/main/scala/core/ArticleAnalyser.scala
@@ -6,6 +6,7 @@ import akka.actor.Actor
   * Created by Paweł Kopeć on 7/9/17.
   */
 class ArticleAnalyser extends Actor {
+  
   override def receive: Receive =  {
     //TODO
     case Analyse(hyperText) =>
