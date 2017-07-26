391edda8795b7124ce7271f6b150d256b4ad69ce
pawelkopec <pawelkopec10@gmail.com>
2017-07-26 11:36:28 +0200

    Add two classes

---
+++ src/main/scala/app/CnnScraper.scala
@@ -0,0 +1,23 @@
+package app
+
+import akka.actor.ActorRef
+import core.Scraper
+
+/**
+  * Created by kopec on 7/18/17.
+  */
+class CnnScraper(analyser: ActorRef) extends Scraper(analyser) {
+
+  def url =
+    "http://edition.cnn.com"
+
+  def linkSelector: String =
+    "ul.cn-list-hierarchical-xs > article.cd--article > " +
+      "div.cd__wrapper > div.cd__content > h3.cd__headline > a"
+
+  def titleSelector: String =
+    "article div h1.pg-headline"
+
+  def contentSelector: String =
+    "article section#body-text"
+}
---
+++ src/main/scala/core/Article.scala
@@ -0,0 +1,6 @@
+package core
+
+/**
+  * Created by kopec on 7/18/17.
+  */
+class Article(val title: String, val content: String)
